22e4299b92bf04437b0c2b4f3d985b56819356e8
Sunnit <edwinnoblemessinger@icloud.com>
2023-11-07 18:26:21 -0800

    test

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+echo setting up
+cd C:\Users\%USERNAME%
+md plymkr
+cd plymkr
+(
+    echo @echo off
+    echo echo curl https://raw.githubusercontent.com/Sunnit-M/plymkr/main/launcherupdate.txt --ssl-no-revoke>install.bat
+    echo echo call install.bat
+    echo echo installing...
+    echo timeout -t 10
+)>plymkr.bat
+mklink C:\Users\%USERNAME%\plymkr\plymkr.bat C:\WINDOWS\system32
