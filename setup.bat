8529a23878137c6b60632ca78ea5015b793aba9b
Sunnit <edwinnoblemessinger@icloud.com>
2023-11-16 18:46:44 -0800

    Test

--- setup.bat
+++ setup.bat
@@ -1,13 +1,17 @@
 @echo off
 echo setting up
 cd C:\Users\%USERNAME%
+del plymkr
 md plymkr
 cd plymkr
 (
     echo @echo off
-    echo curl https://raw.githubusercontent.com/Sunnit-M/plymkr/main/launcherupdate.txt --ssl-no-revoke>install.bat
+    echo curl https://raw.githubusercontent.com/Sunnit-M/plymkr/main/launcherupdate.txt --ssl-no-revoke^>install.bat
     echo call install.bat
     echo echo installing...
     echo timeout -t 10
+    echo del install.bat
 )>plymkr.bat
 mklink C:\Users\%USERNAME%\plymkr\plymkr.bat C:\WINDOWS\system32
+echo the pymkr command is setup now all you have to do is run plymkr in you command prompt in terminal
+pause
